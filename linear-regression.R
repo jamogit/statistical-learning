c645fd1b229cd6583e952320d85e6da2eca4f84f
Jan Moilanen <jan.moilanen@ek.fi>
2022-03-12 23:56:13 +0100

    Sekalaisia testejä

--- linear-regression.R
+++ linear-regression.R
@@ -1,4 +1,5 @@
 library(ISLR)
+library(MASS)
 
 # Lineaarinen regressio ja koneoppinen ----
 
@@ -35,6 +36,10 @@ pairs.panels(Auto[c("mpg", "horsepower")])
 df <- subset(Auto, select = -name)
 cor(df)
 
+### Testataan histogrammia kaikille muuttujille ----
+par(mfrow=c(2,4))
+lapply(df, hist, main = c("Test1", "Test2"))
+
 library(dplyr)
 cor(Auto %>% select(-name))
 
@@ -47,4 +52,91 @@ par(mfrow=c(2,2))
 plot(m)
 
 ### Interaktiomuuttujia ----
-m2 <- lm(mpg~, -name, data = Auto)
+m2 <- lm(mpg~. -name + cylinders*displacement, data = Auto)
+summary(m2)
+
+m3 <- lm(mpg~. -name + cylinders:displacement, data = Auto)
+summary(m3)
+
+### Transformaatio ----
+df.t <- Auto %>% mutate(l_horsepower = log(horsepower))
+m4 <- lm(mpg~. - name, data = df.t)
+summary(m4)
+pairs.panels(df.t)
+
+## Carseats ----
+View(Carseats)
+m.car <- lm(Sales~Price + Urban + US, data = Carseats)
+summary(m.car)
+
+m.car.new <- lm(Sales~Price + US, data=Carseats)
+summary(m.car.new)
+
+### Anova ----
+anova(m.car, m.car.new)
+
+### Luottamusväli ----
+confint(m.car.new)
+
+### Outlierit ----
+par(mfrow=c(2,2))
+plot(m.car.new)
+
+## Malli ilman vakiota ----
+set.seed(1)
+x=rnorm(100)
+y=2*x+rnorm(100)
+
+m.0 <- lm(y~x+0)
+par(mfrow=c(2,2))
+plot(m.0)
+summary(m.0)
+
+### Simuloitua dataa ----
+set.seed(1)
+x <- rnorm(n = 100, mean = 0, sd = 1)
+eps <- rnorm(n = 100, mean = 0, sd = 1/4)
+par(mfrow=c(1,2))
+plot(x)
+plot(eps)
+
+y = -1 + 0.5*x + eps
+m.sim <- lm(y ~ x)
+summary(m.sim)
+
+#### Plotataan malli ----
+plot(x, y)
+abline(m.sim, col = "navyblue", lwd=3)
+legend(-2.5, 0, legend = "Simuloitu\nmalli")
+
+#### Mallin neliömuunnos ----
+m.sim2 <- lm(y ~ poly(x, 2))
+summary(m.sim2)
+
+anova(m.sim, m.sim2)
+
+## Kollineaarisuus ----
+set.seed(1)
+x1 = runif(100)
+x2 = 0.5*x1 + rnorm(100)/10
+y = 2 + 2*x1 + 0.3*x2 + rnorm(100)
+
+cor(x1, x2)
+plot(x1, x2)
+
+m.kol <- lm(y ~ x1 + x2)
+summary(m.kol)
+
+m.kol2 <- lm(y ~ x1)
+summary(m.kol2)
+
+m.kol3 <- lm(y ~ x2)
+summary(m.kol3)
+
+
+## Boston-data ----
+View(Boston)
+?Boston
+
+m.boston <- lm(crim ~ ., data = Boston)
+summary(m.boston)
